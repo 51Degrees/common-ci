f43881eaadbb491c842b87c87393e74095733127
ben51degrees <ben@51degrees.com>
2024-03-18 16:14:45 +0000

    Update run-update-dependencies.ps1

--- dotnet/run-update-dependencies.ps1
+++ dotnet/run-update-dependencies.ps1
@@ -2,10 +2,7 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
     [string]$ProjectDir = ".",
-    [string]$Name,
-    [string]$Source = "https://api.nuget.org/v3/index.json",
-    [string]$SourceUser = "",
-    [string]$SourceKey = ""
+    [string]$Name
 )
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
@@ -24,27 +21,15 @@ try {
             $MinorVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$2' 
             $PatchVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$3' 
 
-            try {
-                if ($SourceUser -ne "" -and $SourceKey -ne "") {
-                    $Password = ConvertTo-SecureString $SourceKey -AsPlainText -Force
-                    $Credential = New-Object System.Management.Automation.PSCredential($SourceUser, $Password)
-                    $Available = $(Find-Package -Name $PackageName -AllVersions -Source $Source -Credential $Credential | Where-Object {$_.Version -Match "^$MajorVersion\.$MinorVersion\..*$"})
-                }
-                else {
-                    $Available = $(Find-Package -Name $PackageName -AllVersions -Source $Source | Where-Object {$_.Version -Match "^$MajorVersion\.$MinorVersion\..*$"})
-                }
-                $HighestPatch = $Available | Sort-Object {[int]($_.Version.Split('.')[2])} | Select-Object -Last 1
-    
-                if ($HighestPatch.Version -ne "$MajorVersion.$MinorVersion.$PatchVersion") {
-    
-                    Write-Output "Updating '$PackageName' from '$MajorVersion.$MinorVersion.$PatchVersion' to $($HighestPatch.Version)"
-    
-                    dotnet add $Project.FullName package $PackageName -v $HighestPatch.Version
-                    
-                }
-            }
-            catch {
-                Write-Output "Could not find the package '$PackageName' in source '$Source' : $_"
+            $Available = $(Find-Package -Name $PackageName -AllVersions -Source https://api.nuget.org/v3/index.json | Where-Object {$_.Version -Match "^$MajorVersion\.$MinorVersion\..*$"})
+            $HighestPatch = $Available | Sort-Object {[int]($_.Version.Split('.')[2])} | Select-Object -Last 1
+
+            if ($HighestPatch.Version -ne "$MajorVersion.$MinorVersion.$PatchVersion") {
+
+                Write-Output "Updating '$PackageName' from '$MajorVersion.$MinorVersion.$PatchVersion' to $($HighestPatch.Version)"
+
+                dotnet add $Project.FullName package $PackageName -v $HighestPatch.Version
+                
             }
         }
     }
