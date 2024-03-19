6328da687141dfe43ce0f0ad896cc92d54495bb0
ben51degrees <ben@51degrees.com>
2024-03-19 14:03:42 +0000

    Update run-update-dependencies.ps1

--- dotnet/run-update-dependencies.ps1
+++ dotnet/run-update-dependencies.ps1
@@ -2,7 +2,8 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
     [string]$ProjectDir = ".",
-    [string]$Name
+    [string]$Name,
+    [scriptblock]$FetchVersions = { param($PackageName) Find-Package -Name $PackageName -AllVersions -Source https://api.nuget.org/v3/index.json }
 )
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
@@ -12,24 +13,29 @@ Push-Location $RepoPath
 
 try {
     
-    dotnet restore $ProjectDir
+    ##dotnet restore $ProjectDir
 
-    foreach ($Project in $(Get-ChildItem -Path $pwd -Filter *.csproj -Recurse -ErrorAction SilentlyContinue -Force)) {
-        foreach ($Package in $(dotnet list $Project.FullName package --outdated | Select-String -Pattern "^\s*>")) {
-            $PackageName = $Package -replace '^ *> ([a-zA-Z0-9\.]*) .*$', '$1' 
-            $MajorVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$1' 
-            $MinorVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$2' 
-            $PatchVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$3' 
+    foreach ($Project in $(Get-ChildItem -Path $pwd -Filter FiftyOne.Pipeline.Core.csproj -Recurse -ErrorAction SilentlyContinue -Force)) {
+        foreach ($Package in $(dotnet list $Project.FullName package | Select-String -Pattern "^\s*>")) {
+            if ($Package.Line.Contains('[') -eq $false -and
+                $Package.Line.Contains(']') -eq $false -and
+                $Package.Line.Contains('(') -eq $false -and
+                $Package.Line.Contains(')'))
+            {
+                $PackageName = $Package -replace '^ *> ([a-zA-Z0-9\.]*) .*$', '$1' 
+                $MajorVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$1' 
+                $MinorVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$2' 
+                $PatchVersion = $Package -replace '^ *> [a-zA-Z0-9\.]* *([0-9]*)\.([0-9]*)\.([0-9]*).*$', '$3' 
 
-            $Available = $(Find-Package -Name $PackageName -AllVersions -Source https://api.nuget.org/v3/index.json | Where-Object {$_.Version -Match "^$MajorVersion\.$MinorVersion\..*$"})
-            $HighestPatch = $Available | Sort-Object {[int]($_.Version.Split('.')[2])} | Select-Object -Last 1
+                $Available = $(&$FetchVersions -PackageName $PackageName | Where-Object {$_.Version -Match "^$MajorVersion\.$MinorVersion\..*$"})
+                $HighestPatch = $Available | Sort-Object {[int]($_.Version.Split('.')[2])} | Select-Object -Last 1
+                if ($null -ne $HighestPatch.Version -and $HighestPatch.Version -ne "$MajorVersion.$MinorVersion.$PatchVersion") {
 
-            if ($HighestPatch.Version -ne "$MajorVersion.$MinorVersion.$PatchVersion") {
+                    Write-Output "Updating '$PackageName' from '$MajorVersion.$MinorVersion.$PatchVersion' to $($HighestPatch.Version)"
 
-                Write-Output "Updating '$PackageName' from '$MajorVersion.$MinorVersion.$PatchVersion' to $($HighestPatch.Version)"
-
-                dotnet add $Project.FullName package $PackageName -v $HighestPatch.Version
-                
+                    dotnet add $Project.FullName package $PackageName -v $HighestPatch.Version
+                    
+                }
             }
         }
     }
