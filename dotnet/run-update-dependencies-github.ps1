396fdb9adce69635d4d21305566de22d658f54dc
ben51degrees <ben@51degrees.com>
2024-03-19 14:04:40 +0000

    Create run-update-dependencies-github.ps1

---
+++ dotnet/run-update-dependencies-github.ps1
@@ -0,0 +1,18 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
+    [string]$OrgName,
+    [string]$ProjectDir = ".",
+    [string]$Name
+)
+
+$FetchVersions = {
+    param($PackageName)
+    gh api `
+       -H "Accept: application/vnd.github+json" `
+       -H "X-GitHub-Api-Version: 2022-11-28" `
+       /orgs/$OrgName/packages/nuget/$PackageName/versions -ErrorAction None | ConvertFrom-Json | ForEach-Object -Process { @{"Version" = $_.name }}
+}
+
+./dotnet/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -FetchVersions $FetchVersions
