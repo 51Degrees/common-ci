42dea4919efe376c41d406dc9ee0b9c2b6b94a8e
Alexander Popel <alexander.popel@postindustria.com>
2025-08-28 11:13:12 +0300

    steps: add fetch-assets script

---
+++ steps/fetch-assets.ps1
@@ -0,0 +1,32 @@
+param (
+    [Parameter(Mandatory)][string[]]$Assets,
+    [string]$DeviceDetection,
+    [string]$DeviceDetectionUrl
+)
+$ErrorActionPreference = "Stop"
+
+$cache = New-Item -ItemType Directory -Path assets -Force
+
+foreach ($asset in $Assets) {
+    if (Test-Path $cache/$asset) {
+        Write-Host "Asset '$asset' already present in cache, skipping download"
+        continue
+    }
+    Write-Host "Fetching '$asset'"
+    switch -Exact -CaseSensitive ($asset) {
+        "TAC-HashV41.hash" {
+            & $PSScriptRoot/fetch-hash-assets.ps1 -RepoName . -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
+            Move-Item -Path $_ -Destination $cache
+        }
+        "51Degrees-LiteV4.1.hash" {
+            Invoke-WebRequest -Uri "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash" -OutFile $cache/$_
+        }
+        "20000 Evidence Records.yml" {
+            Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml" -OutFile $cache/$_
+        }
+        "20000 User Agents.csv" {
+            Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv" -OutFile $cache/$_
+        }
+        default { Write-Error "Unknown asset: $_" }
+    }
+}
