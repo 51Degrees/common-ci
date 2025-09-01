bf24df5328813196d85b626452a6d0522c520f6e
Alexander Popel <alexander.popel@postindustria.com>
2025-09-01 10:48:03 +0300

    fetch-hash-assets: use more reliable relative paths

--- steps/fetch-hash-assets.ps1
+++ steps/fetch-hash-assets.ps1
@@ -8,7 +8,7 @@ param (
 )
 
 Write-Host "Downloading $DataType data file"
-./steps/download-data-file.ps1 -LicenseKey $LicenseKey -DataType $DataType -Product $Product -FullFilePath $RepoName/$ArchiveName -Url $Url
+& $PSScriptRoot/download-data-file.ps1 -LicenseKey $LicenseKey -DataType $DataType -Product $Product -FullFilePath $RepoName/$ArchiveName -Url $Url
 
 Write-Host "Extracting $ArchiveName"
-./steps/gunzip-file.ps1 $RepoName/$ArchiveName
+& $PSScriptRoot/gunzip-file.ps1 $RepoName/$ArchiveName
