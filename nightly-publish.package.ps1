ac1330c9f801d263b332935dcda13a11962946d1
Alexander Popel <alexander.popel@postindustria.com>
2026-01-15 13:01:20 +0200

    Allow custom CI dirs in options.json

--- nightly-publish.package.ps1
+++ nightly-publish.package.ps1
@@ -1,16 +1,12 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
-    [Parameter(Mandatory=$true)]
-    [string]$OrgName,
+    [Parameter(Mandatory)][string]$RepoName,
+    [Parameter(Mandatory)][string]$OrgName,
+    [Parameter(Mandatory)][string]$GitHubToken,
+    [Parameter(Mandatory)][hashtable]$Options,
     [string]$Branch = "main",
-    [Parameter(Mandatory=$true)]
-    [string]$GitHubToken,
     [string]$GitHubUser,
     [string]$GitHubEmail,
-    [Parameter(Mandatory=$true)]
-    [hashtable]$Options,
-    [bool]$DryRun = $False
+    [bool]$DryRun
 )
 $ErrorActionPreference = "Stop"
 
@@ -20,31 +16,31 @@ if ($Options.Keys) {
     $Options += $Options.Keys # Expand keys into options
 }
 
-Write-Output "::group::Configure Git"
+Write-Host "::group::Configure Git"
 ./steps/configure-git.ps1 -GitHubToken $GitHubToken -GitHubUser $GitHubUser -GitHubEmail $GitHubEmail
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Clone $RepoName"
+Write-Host "::group::Clone $RepoName"
 ./steps/clone-repo.ps1 -RepoName $RepoName -OrgName $OrgName -Branch $Branch
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Install Package From Artifact"
+Write-Host "::group::Install Package From Artifact"
 ./steps/run-script.ps1 ./$RepoName/ci/install-package.ps1 $Options
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Publish Packages"
+Write-Host "::group::Publish Packages"
 if ($Branch -ceq "main" -or $Branch -ceq "alpha" -or $Branch -clike "version/*") {
     ./steps/run-script.ps1 ./$RepoName/ci/publish-package.ps1 $Options
 } else {
-    Write-Output "Not on the main branch, skipping publishing"
+    Write-Host "Not on the main branch, skipping publishing"
 }
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Update Tag"
+Write-Host "::group::Update Tag"
 if ($global:SkipUpdateTag) { # Using a global here so that it can be set by publish-package.ps1
-  Write-Output "Tag update skipped"
+  Write-Host "Tag update skipped"
 } else {
   ./steps/update-tag.ps1 -RepoName $RepoName -OrgName $OrgName -Tag $Options.Version -DryRun $DryRun
   ./steps/upload-release-assets.ps1 -RepoName $RepoName -OrgName $OrgName -Tag $Options.Version -DryRun $DryRun
 }
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
--- nightly-publish.test.ps1
+++ nightly-publish.test.ps1
@@ -1,16 +1,12 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
-    [Parameter(Mandatory=$true)]
-    [string]$OrgName,
+    [Parameter(Mandatory)][string]$RepoName,
+    [Parameter(Mandatory)][string]$OrgName,
+    [Parameter(Mandatory)][string]$GitHubToken,
+    [Parameter(Mandatory)][Hashtable]$Options,
     [string]$Branch = "main",
-    [Parameter(Mandatory=$true)]
-    [string]$GitHubToken,
-    [string]$GitHubUser = "",
-    [string]$GitHubEmail = "",
-    [Parameter(Mandatory=$true)]
-    [Hashtable]$Options,
-    [bool]$DryRun = $False
+    [string]$GitHubUser,
+    [string]$GitHubEmail,
+    [bool]$DryRun
 )
 $ErrorActionPreference = "Stop"
 
@@ -19,35 +15,40 @@ if ($Options.Keys) {
     $Options += $Options.Keys # Expand keys into options
 }
 
-Write-Output "::group::Configure Git"
+Write-Host "::group::Configure Git"
 ./steps/configure-git.ps1 -GitHubToken $GitHubToken -GitHubUser $GitHubUser -GitHubEmail $GitHubEmail
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Clone $RepoName"
+Write-Host "::group::Clone $RepoName"
 ./steps/clone-repo.ps1 -RepoName $RepoName -OrgName $OrgName -Branch $Branch
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Fetch Assets"
+if ($Options.CI) {
+    & "./$RepoName/$($Options.CI)/test.ps1" @Options
+    exit
+}
+
+Write-Host "::group::Fetch Assets"
 ./steps/run-script.ps1 ./$RepoName/ci/fetch-assets.ps1 $Options
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Setup Environment"
+Write-Host "::group::Setup Environment"
 ./steps/run-script.ps1 ./$RepoName/ci/setup-environment.ps1 $Options
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Install Package From Artifact"
+Write-Host "::group::Install Package From Artifact"
 ./steps/run-script.ps1 ./$RepoName/ci/install-package.ps1  $Options
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
-Write-Output "::group::Run Integration Tests"
+Write-Host "::group::Run Integration Tests"
 ./steps/run-script.ps1 ./$RepoName/ci/run-integration-tests.ps1 $Options
-Write-Output "::endgroup::"
+Write-Host "::endgroup::"
 
 if ($Options.RunPerformance) {
-    Write-Output "::group::Run Performance Tests"
+    Write-Host "::group::Run Performance Tests"
     ./steps/run-script.ps1 ./$RepoName/ci/run-performance-tests.ps1 $Options
-    Write-Output "::endgroup::"
+    Write-Host "::endgroup::"
 }
 else {
-    Write-Output "Skipping performance tests as they are not configured for '$($Options.Name)'"
+    Write-Host "Skipping performance tests as they are not configured for '$($Options.Name)'"
 }
